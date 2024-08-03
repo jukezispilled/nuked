e38579b7985c028b0c35c39b523f9c9974e37f41
Jack Anderson <whotfisjax@gmail.com>
2024-08-03 15:28:28 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [muted, setMuted] = useState(true);
+  const videoRef = useRef(null);
+
+  const toggleMute = () => {
+    const video = videoRef.current;
+    if (video) {
+      video.muted = !video.muted;
+      setMuted(video.muted);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="h-screen w-screen flex justify-center items-center overflow-hidden relative">
+      <video
+        ref={videoRef}
+        autoPlay
+        loop
+        muted={muted}
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover overflow-hidden"
+        style={{ pointerEvents: 'none' }}
+      >
+        <source src={`${process.env.PUBLIC_URL}/vid1.mp4`} type="video/mp4" />
+      </video>
+      <img
+        src={`${process.env.PUBLIC_URL}/nuked.png`}
+        alt="Nuked"
+        className="absolute nuke-animation overflow-hidden w-[70%] h-auto"
+      />
+      <div className='z-20 flex flex-col items-center'>
+        <div className='text-6xl md:text-[200px] font-custom leading-none tracking-normal text-orange-700'>
+          NUKED
+        </div>
+        <p className='text-sm md:text-xl mt-[-10px] md:mt-[-60px]'><span className='font-custom'>CA</span>: <span className='font-custom'>updating</span>...</p>
+      </div>
+      <button 
+        onClick={toggleMute}
+        className="absolute top-5 left-5 bg-orange-500 p-2 rounded-full z-10"
+      >
+        {muted ? (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+          </svg>        
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+          </svg>
+        )}
+      </button>
+      <div className='absolute bottom-3 right-3 flex space-x-1 items-center z-[50]'>
+        <a href="https://x.com/" className='transition ease-in-out duration-150'>
+          <img src="Xlogo.jpg" className='size-12 rounded-xl'></img>
         </a>
-      </header>
+        <a href="https://t.me/" className='transition ease-in-out duration-150'>
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-14' viewBox="0 0 48 48">
+            <path fill="#29b6f6" d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"></path><path fill="#fff" d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z"></path><path fill="#b0bec5" d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z"></path><path fill="#cfd8dc" d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z"></path>
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
